24bf8cbbe4a91d3cdb4a603749be5e24973e7a8a
federasty <federicodaniel7@gmail.com>
2025-09-18 21:27:42 -0300

    Primer commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CharactersModule } from './characters/characters.module';
+import { CharactersModule } from './modules/characters/characters.module';
 
 @Module({
   imports: [CharactersModule],
